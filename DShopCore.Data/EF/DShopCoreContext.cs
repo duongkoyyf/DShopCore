7023b27c92032f4ce36eb037f42900346825ed6d
duongkoyyf <nguyenvanduong160797@gmail.com>
2021-12-08 11:38:30 +0700

    Add Entity

---
+++ DShopCore.Data/EF/DShopCoreContext.cs
@@ -0,0 +1,20 @@
+﻿using DShopCore.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DShopCore.Data.EF
+{
+    public class DShopCoreContext : DbContext
+    {
+        public DShopCoreContext( DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { set; get; }
+        public DbSet<Category> Categories { set; get; }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 #ASP.NET Core
 DShopCore
+#Technologies
+-ASP.NET Core 3.1
+-Entity Framework Core 3.1
+#Install Tools
+-.NET Core SDK 3.1
+-Git client
+-Visual Studio 2019
+-SQL Server 2019
+#Packages
+-Microsoft.EntityFrameworkCore.Design
+-Microsoft.EntityFrameworkCore.Tool
+-Microsoft.EntityFrameworkCore.SqlServer
